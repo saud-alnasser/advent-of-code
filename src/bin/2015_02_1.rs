9937682fe52739fd8f3e5da385ca8c3fc433dd00
saud alnasser <123410712+saud-alnasser@users.noreply.github.com>
2024-06-05 18:30:26 +0300

    feat: solve puzzle 2015:02:1 and 2015:02:2 (#3)

---
+++ src/bin/2015_02_1.rs
@@ -0,0 +1,49 @@
+aoc::puzzle!("2015:02:1");
+
+struct Gift {
+    l: u32,
+    w: u32,
+    h: u32,
+}
+
+impl Gift {
+    fn area(&self) -> u32 {
+        2 * self.l * self.w + 2 * self.w * self.h + 2 * self.h * self.l
+    }
+
+    fn slack(&self) -> u32 {
+        let mut sides = [self.l, self.w, self.h];
+        sides.sort_unstable();
+
+        sides[0] * sides[1]
+    }
+}
+
+impl Solution for Puzzle {
+    type Input = Vec<Gift>;
+
+    fn parse(input: &str) -> Self::Input {
+        input
+            .lines()
+            .map(|line| {
+                let mut parts = line.split('x');
+
+                Gift {
+                    l: parts.next().unwrap().parse().unwrap(),
+                    w: parts.next().unwrap().parse().unwrap(),
+                    h: parts.next().unwrap().parse().unwrap(),
+                }
+            })
+            .collect()
+    }
+
+    fn solve(gifts: Self::Input) -> Option<String> {
+        Some(
+            gifts
+                .iter()
+                .map(|gift| gift.area() + gift.slack())
+                .sum::<u32>()
+                .to_string(),
+        )
+    }
+}
